9336545951dd0b157aa53e029b68e0592726bac6
Mwanzia Brian <mwanziabyn@gmail.com>
2024-01-23 23:14:25 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 export default function App() {
   return(
-    <div>
+    <div className='App'>
       <Counter/>
     </div>
+    //<div className="App">
+      //<Counter />
+    //</div>
   );
 }
 
@@ -13,20 +16,33 @@ function Counter() {
   const [step, setStep] = useState(1)
   const [count, setCount] = useState(0);
 
-  const date = new Date('December 12 2023');
+  const date = new Date();
   date.setDate(date.getDate() + count);
+
+  function handleClick(){
+    setStep(1)
+    setCount(0)
+  }
 
   return(
     <div>
 
       <div>
-        <button onClick={() => setStep((c) => (c - 1))}>-</button>
+        <input type="range" min="0" max="10" 
+        value = {step} 
+        onChange={e => setStep(Number(e.target.value))}
+        />
+       
         <span>Step: {step}</span>
-        <button onClick={() => setStep((c) => (c + 1))}>+</button>
+        
       </div>
+
       <div>
         <button onClick={() => setCount((c) => (c - step))}>-</button>
-        <span>Count: {count}</span>
+        <input type='text' 
+        value={count} 
+        onChange={e => setCount((Number(e.target.value)))}
+        />
         <button onClick={() => setCount((c) => (c + step))}>+</button>
       </div>
 
@@ -37,6 +53,14 @@ function Counter() {
         `${Math.abs(count)} days ago would be `}
       </span>
       <span>{date.toDateString()}</span>
+     
+      
+      {count !== 0 || step !== 1 ?(
+        <div>
+          <button onClick={handleClick}>Reset</button>
+      </div> 
+      ) : null }
+        
     </div>
   )
 }
